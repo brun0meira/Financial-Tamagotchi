602a3e6335f3d982b24bcc64a0837a40e0870838
Bruno Meira <99202553+brun0meira@users.noreply.github.com>
2023-01-07 17:05:55 -0300

    Fixing game dependencies and bugs

--- src/Script/NameEdit.gd
+++ src/Script/NameEdit.gd
@@ -1,5 +1,4 @@
 extends LineEdit
-
 # Script that redirect from the Name select to the bedroom
 
 export var reference_path = "res://Scenery/bedroom.tscn"
@@ -8,11 +7,14 @@ func _ready():
 	grab_focus()
 	set_cursor_position(len(text))
 
+#redirect player to storytelling
+func _on_ContinueButton_pressed(): # On continue button pressed
+	print(Global.player_name)
+	get_tree().change_scene("res://Scenes/instructions1.tscn")
+
+
 func _on_LineEdit_text_entered(new_text):
+	print(new_text)
 	Global.player_name = new_text
 	print(Global.player_name)
 	get_tree().change_scene(reference_path)
-
-#redirect player to storytelling
-func _on_ContinueButton_pressed(): # On continue button pressed
-	get_tree().change_scene("res://Scenes/instructions1.tscn")
